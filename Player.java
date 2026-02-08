eff2af851a4b47081187deea0efbbdc513bf855e
Aaron <rnzaaron@gmail.com>
2026-02-09 00:08:45 +0800

    PLAYER_CLASS

--- Player.java
+++ Player.java
@@ -1,3 +1,17 @@
 public class Player {
-    
+    private String username;
+    private Inventory inventory;
+
+    public Player(String username) { 
+        this.username = username;
+        this.inventory = new Inventory(3);
+    }
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public String identity() {
+        return "I am the Player class. I represent the player in the game.";
+    }
 }
